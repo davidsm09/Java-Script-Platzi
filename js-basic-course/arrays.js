6e85d529453e6aad04e1ee978a8d1a78e1e000e6
davidcol <mezad55@gmail.com>
2022-06-15 00:10:48 -0500

    Seguimos aprendiendo js

--- js-basic-course/arrays.js
+++ js-basic-course/arrays.js
@@ -2,4 +2,24 @@
 
 var frutas = ["Manzana", "Banana", "Cereza", "Fresa"];
 
-console.log(frutas);
+//console.log(frutas.length); ver longitud
+
+//console.log(frutas[2]); acceder a un elemento del array en especifico
+
+//Metodos
+
+var masFrutas = frutas.push("Uvas"); //Push: Agrega un elemento al final del array
+
+var ultimo = frutas.pop("Uvas");//pop: quitamos el ultimo elemento
+
+var inicio = frutas.unshift("Uvas");//Agrega un elemenmto al inicio del array
+
+var inicioF = frutas.shift("Uvas");//Nos quita el primer elemento
+
+var posicion = frutas.indexOf("Cereza")//nos busca la posicion de un elemento en especifico
+
+
+
+
+
+
